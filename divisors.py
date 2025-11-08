ab1855f233fb0ec17214ce2763e8c6ac908a786d
kimhyunsoo017 <gimhyunsoo0530@gmail.com>
2025-11-09 01:26:20 +0900

    Fix formatting issues in divisors.py

--- divisors.py
+++ divisors.py
@@ -1,9 +1,9 @@
 import sys
 
-number = int(sys.argv[1])   # 명령줄 인자로 숫자 입력받기
+number = int(sys.argv[1])  
 
-for i in range(1, number + 1):  # 1부터 number까지 반복
-    if number % i == 0:         # 나머지가 0이면 약수
-        print(i, end=" ")       # 공백으로 구분하여 출력
+for i in range(1, number + 1):  
+    if number % i == 0:         
+        print(i, end=” “)       
 
-print()  # 마지막 줄바꿈
+print()  
